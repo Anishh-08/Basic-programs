d8ceab751ee6423025de27480fe2f10efa61dd32
Anishh-08 <anish.annambhotla1@gmail.com>
2025-09-02 22:53:48 +0530

    FINAL

--- temp-conversion/tempconv.py
+++ temp-conversion/tempconv.py
@@ -1,10 +1,10 @@
 temp_input=str(input("Which scale do you want to enter the temperature in (only celcius): "))
 temp=float(input('Enter temperature: '))
 if temp_input == 'Celcius':
-    conv=float(input('Which scale do you want to convert into: '))
+    conv=str(input('Which scale do you want to convert into: '))
     if conv == 'Kelvin':
         C_K= temp + 273
-        print('In terms of kelvin: {C_K}')
+        print(f'In terms of kelvin: {C_K}')
     if conv == 'Farenheit':
         C_F= (temp*1.8)+32
         print(f'In terms of Farenheit: {C_F}')
